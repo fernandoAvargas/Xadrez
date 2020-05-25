55d51dd01a32c7a80ce379190ff5d4b523b11345
fernandoAvargas <fernan.assis@uol.com.br>
2020-05-25 14:07:26 -0300

    Melhorando a implementação do método ImprimirTabuleiro!

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -11,36 +11,39 @@ namespace xadrez_console
         {
 
 
-            PosicaoXadrez pos1 = new PosicaoXadrez('a', 1);
+            //PosicaoXadrez pos1 = new PosicaoXadrez('a', 1);
 
 
-            Console.WriteLine(pos1);
+            //Console.WriteLine(pos1);
 
-            Console.WriteLine(pos1.ToPosicao());
+            //Console.WriteLine(pos1.ToPosicao());
 
-            PosicaoXadrez pos2 = new PosicaoXadrez('b', 7);
+            //PosicaoXadrez pos2 = new PosicaoXadrez('b', 7);
 
-            Console.WriteLine(pos2.ToPosicao());
+            //Console.WriteLine(pos2.ToPosicao());
 
-            PosicaoXadrez pos3 = new PosicaoXadrez('c', 7);
+            //PosicaoXadrez pos3 = new PosicaoXadrez('c', 7);
 
-            Console.WriteLine(pos3.ToPosicao());
+            //Console.WriteLine(pos3.ToPosicao());
 
 
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
-            //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));   
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            //    Tela.ImprimirTabuleiro(tab);
-            //}
-            //catch (TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+                tab.ColocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                tab.ColocarPeca(new Cavalo(tab, Cor.Branca), new Posicao(7, 2));
+
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadLine();           
         }
--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -11,6 +11,8 @@ namespace xadrez_console
 
             for(int i=0;i<tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
+
                 for(int j=0;j<tab.Colunas; j++)
                 {
 
@@ -22,7 +24,8 @@ namespace xadrez_console
                     else
                     {
 
-                        Console.Write(tab.Peca(i,j) + " ");
+                        ImprimirPeca(tab.Peca(i, j));
+                        Console.Write(" ");
 
                     }                  
 
@@ -31,6 +34,27 @@ namespace xadrez_console
                 Console.WriteLine();
 
             }
+
+            Console.WriteLine("  A B C D E F G H");
+
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if(peca.Cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
+
+            }
+
 
         }
 
