3b866ec7d0134964f8f3b80cdc9e390b8ad161f5
fernandoAvargas <fernan.assis@uol.com.br>
2020-05-25 12:48:03 -0300

    Cração da classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 using xadrez_console.xadrez;
 
 namespace xadrez_console
@@ -8,20 +9,38 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));   
 
-                Tela.ImprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            PosicaoXadrez pos1 = new PosicaoXadrez('a', 1);
+
+
+            Console.WriteLine(pos1);
+
+            Console.WriteLine(pos1.ToPosicao());
+
+            PosicaoXadrez pos2 = new PosicaoXadrez('b', 7);
+
+            Console.WriteLine(pos2.ToPosicao());
+
+            PosicaoXadrez pos3 = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos3.ToPosicao());
+
+
+            //try
+            //{
+            //    Tabuleiro tab = new Tabuleiro(8, 8);
+
+            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            //    tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 2));
+            //    tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));   
+
+            //    Tela.ImprimirTabuleiro(tab);
+            //}
+            //catch (TabuleiroException e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
 
             Console.ReadLine();           
         }
